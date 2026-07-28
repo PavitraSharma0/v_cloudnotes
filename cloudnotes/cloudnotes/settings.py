208204d60397d786d5642ed15002fb8bf343e728
Pavitrs Sharma <spavitra9211@gmail.com>
2026-07-29 00:45:44 +0530

    Allow Vercel hosts

--- cloudnotes/cloudnotes/settings.py
+++ cloudnotes/cloudnotes/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-+!6o2osuw8id^)utcm(mztx1mg8n0oj$rgba98o-$bo7*1ux!n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".vercel.app",
+]
 
 
 # Application definition
